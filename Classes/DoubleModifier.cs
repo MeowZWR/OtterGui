11b72e7934764f5a2ade0b45cb4c33416c17fa0e
MeowZ#WR <antabakala@hotmail.com>
2024-10-27 11:35:38 +0800

    更新 DoubleModifier.cs

--- Classes/DoubleModifier.cs
+++ Classes/DoubleModifier.cs
@@ -74,7 +74,7 @@ public struct DoubleModifier : IEquatable<DoubleModifier>
 
     public override string ToString()
         => Modifier2 != ModifierHotkey.NoKey
-            ? $"{Modifier1}和{Modifier2}"
+            ? $"{Modifier1} 和 {Modifier2}"
             : Modifier1.ToString();
 
     public bool IsActive()
